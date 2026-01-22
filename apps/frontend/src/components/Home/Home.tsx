fffafb3c1a92d1ec50ab492fa6d0a309c364f0bc
rashedrk <rashedrk001@gmail.com>
2026-01-22 16:01:03 +0600

    fix (userCard): use card component for better ui

--- apps/frontend/src/components/Home/Home.tsx
+++ apps/frontend/src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "../ui/separator";
 import SearchBox from "./SearchBox/SearchBox";
 import UserCard from "./UserCard/UserCard";
+import UserDetails from "./UserDetails/UserDetails";
 
 const Home = () => {
     return (
@@ -14,7 +15,7 @@ const Home = () => {
                     <UserCard/>
                 </div>
                 <Separator orientation="vertical"/>
-                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quod, voluptatem perspiciatis tempora debitis nostrum error ut officia iure quibusdam iusto dolore harum, in eum, iste molestias nobis magni explicabo voluptatibus magnam. Omnis, autem unde repellendus perferendis maxime similique, dolorum labore aperiam cumque, saepe cum reprehenderit at illo quisquam a accusamus nulla nam dignissimos consequuntur tempora temporibus. Quia exercitationem incidunt nulla repellendus repellat saepe iste corporis unde porro ut quas animi, sed optio! Necessitatibus consequatur dignissimos corrupti, reprehenderit voluptatum deleniti quas consequuntur laborum id reiciendis beatae temporibus numquam, ad adipisci quasi quae laboriosam aliquid labore illo rerum eligendi? Aut magnam amet, optio, animi architecto quae aliquid sed quos modi odit repellendus, eveniet excepturi. Ipsa eum temporibus nihil quisquam. Dignissimos consequatur doloribus temporibus labore perspiciatis a cumque corporis possimus? Possimus numquam nam, ipsum sint accusamus deleniti quae debitis qui dicta, quod sunt architecto doloribus est illo.</div>
+                <UserDetails/>
             </div>
         </div>
     );
--- apps/frontend/src/components/Home/UserCard/UserCard.tsx
+++ apps/frontend/src/components/Home/UserCard/UserCard.tsx
@@ -1,31 +1,32 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent } from "@/components/ui/card";
 
 const UserCard = () => {
   return (
-    <div className="border rounded-lg p-4 hover:shadow-md transition-shadow w-80">
-      <div className="flex items-start gap-4">
+    <Card className="w-xs mb-2 hover:shadow-lg cursor-pointer">
+      <CardContent className="flex items-start gap-4">
         <Avatar className="h-12 w-12">
           <AvatarImage src="" alt="" />
-          <AvatarFallback>Name</AvatarFallback>
+          <AvatarFallback>RK</AvatarFallback>
         </Avatar>
         <div className="flex-1">
-          <h3 className="font-semibold text-base mb-4">Rashedul Islam</h3>
+          <h3 className="font-semibold text-base mb-1">Rashedul Islam</h3>
           <div className="text-xs flex items-center justify-between gap-2">
             <p className="px-2 py-1 bg-secondary rounded-md capitalize">
               Admin
             </p>
-            <p className="text-gray-700">
+            <p className="text-gray-400">
               <span
                 className={`inline-flex h-2 w-2 rounded-full mr-2 ${
                   true ? "bg-green-500" : "bg-gray-400"
                 }`}
               />
-              {true ? "Active" : "Inactive"}{" "}
+              {true ? "Active" : "Inactive"}
             </p>
           </div>
         </div>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 };
 
